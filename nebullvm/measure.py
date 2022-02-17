868cde9a4a0b82d4b721ea072cd0a696884a2edd
morgoth95 <diego.fiori@epfl.ch>
2022-02-17 11:14:46 +0100

    fix minor bug

--- nebullvm/measure.py
+++ nebullvm/measure.py
@@ -8,24 +8,24 @@ from nebullvm.inference_learners.base import BaseInferenceLearner
 
 
 def compute_torch_latency(
-    x: torch.Tensor,
+    xs: List[torch.Tensor],
     model: torch.nn.Module,
     device: str,
     steps: int,
 ) -> Tuple[float, List[float]]:
-    x = x.to(device)
+    xs = [x.to(device) for x in xs]
     model = model.to(device)
     latencies = []
     for _ in range(steps):
         starting_time = time.time()
-        _ = model.forward(x)
+        _ = model.forward(*xs)
         latencies.append(time.time() - starting_time)
     latency = sum(latencies) / steps
     return latency, latencies
 
 
 def compute_tf_latency(
-    x: tf.Tensor,
+    xs: List[tf.Tensor],
     model: Union[tf.Module, tf.keras.Model],
     device: str,
     steps: int,
@@ -34,7 +34,7 @@ def compute_tf_latency(
     with tf.device(device):
         for _ in range(steps):
             starting_time = time.time()
-            _ = model(x)
+            _ = model(*xs)
             latencies.append(time.time() - starting_time)
         latency = sum(latencies) / steps
         return latency, latencies
