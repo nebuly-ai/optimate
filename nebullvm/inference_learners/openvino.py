718fa70dcf15638accf0374585a41c14ec11e885
morgoth95 <diego.fiori@epfl.ch>
2022-03-09 13:34:21 +0100

    fix error in openvino model-reshaping

--- nebullvm/inference_learners/openvino.py
+++ nebullvm/inference_learners/openvino.py
@@ -138,10 +138,10 @@ class OpenVinoInferenceLearner(BaseInferenceLearner, ABC):
 
     def _rebuild_network(self, input_shapes: Dict):
         inference_engine = IECore()
-        # network = inference_engine.read_network(
-        #     model=self.description_file, weights=self.weights_file
-        # )
-        network = self.exec_network.get_exec_graph_info()
+        network = inference_engine.read_network(
+            model=self.description_file, weights=self.weights_file
+        )
+        # network = self.exec_network.get_exec_graph_info()
         if all(
             input_shape == tuple(network.inputs[input_name].shape)
             for input_name, input_shape in input_shapes.items()
