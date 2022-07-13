96779d266d587eceabe4a3d40d91e836bd41e5c0
morgoth95 <d.fiori@nebuly.ai>
2022-07-13 17:54:29 +0200

    fix API error with Feedback collection, add nebullvm version in config and fix requirements

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from pathlib import Path
 from setuptools import setup, find_packages
 
+
 REQUIREMENTS = [
     "numpy>=1.20.0, <1.23.0",
     "scipy<=1.5.4",
     "onnx>=1.10.0",
     "onnxmltools>=1.11.0",
     "py-cpuinfo>=8.0.0",
+    "psutil>=5.9.0",
+    "requests>=2.28.1",
     "tensorflow>=2.7.0",
     "tf2onnx>=1.8.4",
     "torch>=1.10.0",
     "tqdm>=4.63.0",
 ]
+
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
