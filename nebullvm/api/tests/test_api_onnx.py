cf16cd90d6031d360a5cf0e8e20529834c2fa80a
Valerio Sofi <valeriosofi@MBP-di-Valerio.homenet.telecomitalia.it>
2022-09-18 15:07:57 +0200

    added tests for tensorflow and onnx

---
+++ nebullvm/api/tests/test_api_onnx.py
@@ -0,0 +1,176 @@
+import cpuinfo
+from tempfile import TemporaryDirectory
+
+import numpy as np
+import torch
+from torchvision import models
+
+from nebullvm.api.functions import optimize_model
+from nebullvm.api.tests.utils import torch_to_onnx
+from nebullvm.inference_learners.onnx import NumpyONNXInferenceLearner
+from nebullvm.inference_learners.tensor_rt import (
+    NumpyNvidiaInferenceLearner,
+)
+from nebullvm.inference_learners.openvino import (
+    NumpyOpenVinoInferenceLearner,
+)
+from nebullvm.inference_learners.tvm import NumpyApacheTVMInferenceLearner
+from nebullvm.utils.compilers import (
+    tvm_is_available,
+)
+
+
+def test_onnx_onnx():
+    with TemporaryDirectory() as tmp_dir:
+        model = models.resnet18().eval()
+        input_data = [((torch.randn(1, 3, 256, 256),), 0) for i in range(100)]
+        model_path = torch_to_onnx(model, input_data, tmp_dir)
+
+        input_data = [
+            ((np.random.randn(1, 3, 256, 256).astype(np.float32),), 0)
+            for i in range(100)
+        ]
+
+        # Run nebullvm optimization in one line of code
+        optimized_model = optimize_model(
+            model_path,
+            input_data=input_data,
+            ignore_compilers=[
+                "deepsparse",
+                "tvm",
+                "torchscript",
+                "tensor RT",
+                "openvino",
+                "bladedisc",
+            ],
+        )
+
+        # Try the optimized model
+        x = torch.randn(1, 3, 256, 256, requires_grad=False)
+        res_original = model(x)
+        res_optimized = optimized_model(x.numpy())[0]
+
+        assert isinstance(optimized_model, NumpyONNXInferenceLearner)
+        assert (
+            abs((res_original.detach().numpy() - res_optimized)).max() < 1e-5
+        )
+
+
+def test_onnx_tensorrt():
+    if not torch.cuda.is_available():
+        # no need of testing the tensor rt optimizer on devices not
+        # supporting CUDA.
+        return
+
+    with TemporaryDirectory() as tmp_dir:
+        model = models.resnet18().eval()
+        input_data = [((torch.randn(1, 3, 256, 256),), 0) for i in range(100)]
+        model_path = torch_to_onnx(model, input_data, tmp_dir)
+
+        input_data = [
+            ((np.random.randn(1, 3, 256, 256).astype(np.float32),), 0)
+            for i in range(100)
+        ]
+
+        # Run nebullvm optimization in one line of code
+        optimized_model = optimize_model(
+            model_path,
+            input_data=input_data,
+            ignore_compilers=[
+                "deepsparse",
+                "tvm",
+                "torchscript",
+                "onnxruntime",
+                "openvino",
+                "bladedisc",
+            ],
+        )
+
+        # Try the optimized model
+        x = torch.randn(1, 3, 256, 256, requires_grad=False)
+        res_original = model(x)
+        res_optimized = optimized_model(x.numpy())[0]
+
+        assert isinstance(optimized_model, NumpyNvidiaInferenceLearner)
+        assert (
+            abs((res_original.detach().numpy() - res_optimized)).max() < 1e-5
+        )
+
+
+def test_onnx_openvino():
+    processor = cpuinfo.get_cpu_info()["brand_raw"].lower()
+    if "intel" not in processor:
+        return
+
+    with TemporaryDirectory() as tmp_dir:
+        model = models.resnet18().eval()
+        input_data = [((torch.randn(1, 3, 256, 256),), 0) for i in range(100)]
+        model_path = torch_to_onnx(model, input_data, tmp_dir)
+
+        input_data = [
+            ((np.random.randn(1, 3, 256, 256).astype(np.float32),), 0)
+            for i in range(100)
+        ]
+
+        # Run nebullvm optimization in one line of code
+        optimized_model = optimize_model(
+            model_path,
+            input_data=input_data,
+            ignore_compilers=[
+                "deepsparse",
+                "tvm",
+                "torchscript",
+                "onnxruntime",
+                "tensor RT",
+                "bladedisc",
+            ],
+        )
+
+        # Try the optimized model
+        x = torch.randn(1, 3, 256, 256, requires_grad=False)
+        res_original = model(x)
+        res_optimized = optimized_model(x.numpy())[0]
+
+        assert isinstance(optimized_model, NumpyOpenVinoInferenceLearner)
+        assert (
+            abs((res_original.detach().numpy() - res_optimized)).max() < 1e-5
+        )
+
+
+def test_onnx_tvm():
+    if not tvm_is_available():
+        return None
+
+    with TemporaryDirectory() as tmp_dir:
+        model = models.resnet18().eval()
+        input_data = [((torch.randn(1, 3, 256, 256),), 0) for i in range(100)]
+        model_path = torch_to_onnx(model, input_data, tmp_dir)
+
+        input_data = [
+            ((np.random.randn(1, 3, 256, 256).astype(np.float32),), 0)
+            for i in range(100)
+        ]
+
+        # Run nebullvm optimization in one line of code
+        optimized_model = optimize_model(
+            model_path,
+            input_data=input_data,
+            ignore_compilers=[
+                "deepsparse",
+                "tensor RT",
+                "torchscript",
+                "onnxruntime",
+                "openvino",
+                "bladedisc",
+            ],
+        )
+
+        # Try the optimized model
+        x = torch.randn(1, 3, 256, 256, requires_grad=False)
+        res_original = model(x)
+        res_optimized = optimized_model(x.numpy())[0]
+
+        assert isinstance(optimized_model, NumpyApacheTVMInferenceLearner)
+        assert (
+            abs((res_original.detach().numpy() - res_optimized)).max() < 1e-5
+        )
---
+++ nebullvm/api/tests/test_api_tensorflow.py
@@ -0,0 +1,146 @@
+import cpuinfo
+
+import tensorflow as tf
+import torch
+from tensorflow.keras.applications.resnet50 import ResNet50
+
+from nebullvm.api.functions import optimize_model
+from nebullvm.inference_learners.onnx import TensorflowONNXInferenceLearner
+from nebullvm.inference_learners.tensor_rt import (
+    TensorflowNvidiaInferenceLearner,
+)
+from nebullvm.inference_learners.openvino import (
+    TensorflowOpenVinoInferenceLearner,
+)
+from nebullvm.inference_learners.tvm import TensorflowApacheTVMInferenceLearner
+from nebullvm.utils.compilers import (
+    tvm_is_available,
+)
+
+
+def test_tensorflow_onnx():
+    model = ResNet50()
+    input_data = [
+        ((tf.random.normal([1, 224, 224, 3]),), 0) for i in range(100)
+    ]
+
+    # Run nebullvm optimization in one line of code
+    optimized_model = optimize_model(
+        model,
+        input_data=input_data,
+        ignore_compilers=[
+            "deepsparse",
+            "tvm",
+            "torchscript",
+            "tensor RT",
+            "openvino",
+            "bladedisc",
+        ],
+    )
+
+    # Try the optimized model
+    x = tf.random.normal([1, 224, 224, 3])
+    res_original = model(x)
+    res_optimized = optimized_model(x)[0]
+
+    assert isinstance(optimized_model, TensorflowONNXInferenceLearner)
+    assert abs((res_original - res_optimized)).numpy().max() < 1e-5
+
+
+def test_tensorflow_tensorrt():
+    if not torch.cuda.is_available():
+        # no need of testing the tensor rt optimizer on devices not
+        # supporting CUDA.
+        return
+
+    model = ResNet50()
+    input_data = [
+        ((tf.random.normal([1, 224, 224, 3]),), 0) for i in range(100)
+    ]
+
+    # Run nebullvm optimization in one line of code
+    optimized_model = optimize_model(
+        model,
+        input_data=input_data,
+        ignore_compilers=[
+            "deepsparse",
+            "tvm",
+            "torchscript",
+            "onnxruntime",
+            "openvino",
+            "bladedisc",
+        ],
+    )
+
+    # Try the optimized model
+    x = tf.random.normal([1, 224, 224, 3])
+    res_original = model(x)
+    res_optimized = optimized_model(x)[0]
+
+    assert isinstance(optimized_model, TensorflowNvidiaInferenceLearner)
+    assert abs((res_original - res_optimized)).numpy().max() < 1e-5
+
+
+def test_tensorflow_openvino():
+    processor = cpuinfo.get_cpu_info()["brand_raw"].lower()
+    if "intel" not in processor:
+        return
+
+    model = ResNet50()
+    input_data = [
+        ((tf.random.normal([1, 224, 224, 3]),), 0) for i in range(100)
+    ]
+
+    # Run nebullvm optimization in one line of code
+    optimized_model = optimize_model(
+        model,
+        input_data=input_data,
+        ignore_compilers=[
+            "deepsparse",
+            "tvm",
+            "torchscript",
+            "onnxruntime",
+            "tensor RT",
+            "bladedisc",
+        ],
+    )
+
+    # Try the optimized model
+    x = tf.random.normal([1, 224, 224, 3])
+    res_original = model(x)
+    res_optimized = optimized_model(x)[0]
+
+    assert isinstance(optimized_model, TensorflowOpenVinoInferenceLearner)
+    assert abs((res_original - res_optimized)).numpy().max() < 1e-5
+
+
+def test_tensorflow_tvm():
+    if not tvm_is_available():
+        return None
+
+    model = ResNet50()
+    input_data = [
+        ((tf.random.normal([1, 224, 224, 3]),), 0) for i in range(100)
+    ]
+
+    # Run nebullvm optimization in one line of code
+    optimized_model = optimize_model(
+        model,
+        input_data=input_data,
+        ignore_compilers=[
+            "deepsparse",
+            "openvino",
+            "torchscript",
+            "onnxruntime",
+            "tensor RT",
+            "bladedisc",
+        ],
+    )
+
+    # Try the optimized model
+    x = tf.random.normal([1, 224, 224, 3])
+    res_original = model(x)
+    res_optimized = optimized_model(x)[0]
+
+    assert isinstance(optimized_model, TensorflowApacheTVMInferenceLearner)
+    assert abs((res_original - res_optimized)).numpy().max() < 1e-5
