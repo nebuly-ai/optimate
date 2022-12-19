1f1d220b12d1db2f9a855771737ad9f4d8d2c0e8
morgoth95 <d.fiori@nebuly.ai>
2022-12-19 15:27:00 +0100

    add forward forward app

---
+++ apps/accelerate/forward_forward/forward_forward/__init__.py
@@ -0,0 +1,3 @@
+from forward_forward.api.functions import (  # noqa F401
+    train_with_forward_forward_algorithm,
+)
---
+++ apps/accelerate/forward_forward/forward_forward/operations/fetch_operations.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+from torch.utils.data import DataLoader
+
+from nebullvm.operations.base import Operation
+
+
+class FetchTrainingDataFromLocal(Operation):
+    def get_result(self) -> Any:
+        pass
+
+    def execute(self, train_data: DataLoader, test_data: DataLoader):
+        self.state["train_data"] = train_data
+        self.state["test_data"] = test_data
+
+    def get_train_data(self) -> DataLoader:
+        return self.state.get("train_data")
+
+    def get_test_data(self) -> DataLoader:
+        return self.state.get("test_data")
