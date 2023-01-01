f45ddedfcdaae3bc07d09c5b5ec45f29d56e2eda
diegofiori <d.fiori@nebuly.ai>
2023-01-01 19:15:59 +0100

    update speedster version

--- apps/accelerate/speedster/speedster/root_op.py
+++ apps/accelerate/speedster/speedster/root_op.py
@@ -68,7 +68,7 @@ from speedster.utils import (
 SPEEDSTER_FEEDBACK_COLLECTOR = FeedbackCollector(
     url="https://nebuly.cloud/v1/store_speedster_results",
     disable_telemetry_environ_var="SPEEDSTER_DISABLE_TELEMETRY",
-    app_version="0.0.1",
+    app_version="0.0.2",
 )
 
 
