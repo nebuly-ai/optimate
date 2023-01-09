b27c09fbca6e32d724c9be6125455668a8eccd48
valerio <v.sofi@nebuly.ai>
2023-01-09 12:08:09 +0100

    upgrade version numbers

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf8")
 
 setup(
     name="nebullvm",
-    version="0.6.1",
+    version="0.7.0",
     packages=find_packages(),
     install_requires=REQUIREMENTS,
     long_description=long_description,
