71ffafd4e83d309360a696d45dc8a3dd52f69f72
diegofiori <d.fiori@nebuly.ai>
2023-01-12 09:34:09 +0100

    release 0.7.2

--- apps/accelerate/speedster/setup.py
+++ apps/accelerate/speedster/setup.py
@@ -12,7 +12,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf8")
 
 setup(
     name="speedster",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=REQUIREMENTS,
     long_description=long_description,
