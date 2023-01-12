8c5eb5bf1f400f0df15ec9ee7eef3cd8b436ea8b
diegofiori <d.fiori@nebuly.ai>
2023-01-13 00:06:32 +0100

    update version

--- nebullvm/config.py
+++ nebullvm/config.py
@@ -1,7 +1,7 @@
 from nebullvm.optional_modules.torch import torch
 
 
-VERSION = "0.7.2"
+VERSION = "0.7.3"
 LEARNER_METADATA_FILENAME = "metadata.json"
 ONNX_OPSET_VERSION = 13
 NEBULLVM_DEBUG_FILE = "nebullvm_debug.json"
