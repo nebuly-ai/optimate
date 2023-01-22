cb782a86f17691feaa3065a173314eaa732436ee
diegofiori <d.fiori@nebuly.ai>
2023-01-22 16:13:13 +0100

    Add save_model function

--- nebullvm/operations/optimizations/utils.py
+++ nebullvm/operations/optimizations/utils.py
@@ -3,6 +3,7 @@ from typing import Callable, List, Union
 
 from nebullvm.operations.inference_learners.base import LearnerMetadata
 
+
 def map_compilers_and_compressors(ignore_list: List, enum_class: Callable):
     if ignore_list is None:
         ignore_list = []
@@ -10,5 +11,10 @@ def map_compilers_and_compressors(ignore_list: List, enum_class: Callable):
         ignore_list = [enum_class(element) for element in ignore_list]
     return ignore_list
 
+
 def load_model(path: Union[Path, str]):
     return LearnerMetadata.read(path).load_model(path)
+
+
+def save_model(model, path: Union[Path, str]):
+    model.save(path)
