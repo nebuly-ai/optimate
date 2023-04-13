604f29520cf1ebf97c264a3d840323ef4863205e
valerio <v.sofi@nebuly.ai>
2023-04-13 10:45:45 +0200

    Add tests

--- apps/accelerate/speedster/speedster/api/tests/test_pytorch.py
+++ apps/accelerate/speedster/speedster/api/tests/test_pytorch.py
@@ -263,7 +263,7 @@ def test_torch_bladedisc():
     # Try the optimized model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     x = torch.randn(1, 3, 256, 256, requires_grad=False).to(device)
-    model.eval()
+    model.to(device).eval()
     res_original = model(x)
     res_optimized = optimized_model(x)[0]
 
